7376f957b7966a740eb98e3c4db2d5b2d022771c
timydaley <tdaley@stanford.edu>
2017-02-27 09:12:47 -0800

    update verbose

--- map_guides.cpp
+++ map_guides.cpp
@@ -624,8 +624,10 @@ main(const int argc, const char **argv) {
         cerr << "processed " << i << " reads" << endl;
     }
     
-    if(VERBOSE)
+    if(VERBOSE){
       cerr << "# matches found = " << n_matches << endl;
+      cerr << "percent mapped = " << n_matches/sequences.size() << endl;
+    }
     write_guides(counts_file_name, guides);
     
   }
